229b143e8c6b6204e50cf2af5040b54eae5c5efd
BSshishodia <bhavshishodia@gmail.com>
2026-03-27 14:48:22 +0530

    Removed unused files and improved structure

--- audio_video_input.py
+++
@@ -1,7 +0,0 @@
-import moviepy.editor as mp
-import os
-
-def extract_audio_from_video(video_path, output_audio_path="extracted_audio.wav"):
-    video = mp.VideoFileClip(video_path)
-    video.audio.write_audiofile(output_audio_path)
-    return output_audio_path
